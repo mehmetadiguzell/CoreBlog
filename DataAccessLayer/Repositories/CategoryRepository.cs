6a19e317e8c39887c9d3291ccb13b72f547ef0bf
mehmetadiguzell <76685004+mehmetadiguzell@users.noreply.github.com>
2022-05-09 18:46:39 +0300

    ASPNET CORE 5

---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,58 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        public void Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategry(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            using (Context db=new Context())
+            {
+                db.Add(entity);
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            using (Context db = new Context())
+            {
+                db.Remove(entity);
+            }
+        }
+
+        public List<T> GetAll()
+        {
+            using (Context db = new Context())
+            {
+                return db.Set<T>().ToList();
+            }
+            
+        }
+
+        public T GetById(int id)
+        {
+            using (Context db = new Context())
+            {
+                return db.Set<T>().Find(id);
+            }
+        }
+
+        public void Update(T entity)
+        {
+            using (Context db = new Context())
+            {
+                db.Update(entity);
+            }
+        }
+    }
+}
